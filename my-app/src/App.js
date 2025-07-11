71a4f55be12d734fc441d4caf9b826b4b1c97ae0
aman <amanbaye4@gmail.com>
2025-07-11 09:22:55 +0300

    all the other files and dependencies

---
+++ my-app/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+// import Greet from './components/Greet'
+// import Welcome from './components/welcome'
+// import Message from './components/Message'
+// import Counter from './components/Counter'
+// import FunctionClick from './components/FunctionClick'
+import ClassClick from './components/ClassClick'
+function App() {
+  return (  
+    <div className="App">
+      <ClassClick />
+       {/* <FunctionClick /> */}
+      {/* <Counter/> */}
+      {/* <Message/> */}
+      {/* <Greet name="markos" heroName="batman" />
+      <p>this is children props</p>
+      <Greet name="eyob" heroName="superman" />
+      <Greet name="abrham" heroName="spiderman"/>
+      <Welcome /> */}
+
+     
+    </div>
+  );
+}
+
+export default App;
