71089f157c147ca80f9dca8ab1b8a52ca0ee0817
aman <amanbaye4@gmail.com>
2025-07-11 08:53:07 +0300

    my first react exersice

---
+++ my-app/src/components/Greet.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>Hello AMAN</h1>
+// }
+
+const Greet = (props) =>{ 
+    console.log(props)
+    return (
+    <div> <h1>Hellllooo {props.name} a.k.a {props.heroName} </h1>
+    {props.children}
+    </div>
+    )
+}
+
+export default Greet
